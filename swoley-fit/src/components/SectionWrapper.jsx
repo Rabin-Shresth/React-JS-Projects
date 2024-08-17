9927f7ad4ea04dc38f48c8fb606cacda77dad3b1
Rabin-Shresth <shrestha.rabin1700@gmail.com>
2024-08-18 00:51:07 +0545

    Component's sub components interaction

--- swoley-fit/src/components/SectionWrapper.jsx
+++ swoley-fit/src/components/SectionWrapper.jsx
@@ -8,7 +8,9 @@ export default function SectionWrapper(props) {
             <p className='uppercase font-medium'>{header}</p>
             <h2 className='font-semibold text-3xl sm:text-4xl md:text-5xl lg:text-6xl '>{title[0]} <span className='uppercase text-blue-400'>{title[1]}</span> {title[2]}</h2>
         </div>
-        {children}
+        <div className='max-w-[800px] w-full flex flex-col mx-auto gap-10 p-4'>
+          {children}
+        </div>
     </section>
   )
 }
