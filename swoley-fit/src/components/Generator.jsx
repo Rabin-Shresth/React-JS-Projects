2a33ac9355c359a95a448877a15aed75814d0698
Rabin-Shresth <shrestha.rabin1700@gmail.com>
2024-08-17 14:38:56 +0545

    next components to be made..

--- swoley-fit/src/components/Generator.jsx
+++ swoley-fit/src/components/Generator.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import SectionWrapper from './SectionWrapper'
+
+function Header(props) {
+  const { index, title, description }= props
+  return (
+    <div>
+      <div>
+        <p>{index}</p>
+        <h4>{title}</h4>
+      </div>
+      <p>{description}</p>
+    </div>
+  )
+}
 
 export default function Generator() {
   return (
-    <div className='min-h-screen'>Generator</div>
+    <SectionWrapper header={"generate your workout"} title={['It\'s', 'Huge', 'o\'clock']}>
+      <Header index={'01'} title={'Pick Your Poison'}  description={'Select the Workout You wish to enjoy'}/>
+    </SectionWrapper>
   )
 }
