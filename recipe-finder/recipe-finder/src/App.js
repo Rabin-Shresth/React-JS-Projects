8b1c3e7b7e6490494fb24dbda7f5423d36a7d0f4
Rabin-Shresth <shrestha.rabin1700@gmail.com>
2024-08-31 15:58:09 +0545

    Parent and children component better understanding using props

--- recipe-finder/recipe-finder/src/App.js
+++ recipe-finder/recipe-finder/src/App.js
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import RecipeList from './components/RecipeList';
 import SearchForm from './components/SearchForm';
 
 function App() {
+
+  const[search, setSearch]=useState('');
+
   return (
     <div className="App">
       <Header />
-      <SearchForm />
+      <SearchForm search={search} setSearch={setSearch}/>
       <RecipeList />
     </div>
   );
--- recipe-finder/recipe-finder/src/components/SearchForm.js
+++ recipe-finder/recipe-finder/src/components/SearchForm.js
@@ -1,21 +1,19 @@
-import React, { useState } from 'react'
 
-export default function SearchForm() {
-  const[search, setSearch]=useState('');
-
+export default function SearchForm(props) {
+  
   //Handle the change in search input
   const handleInputChange =(e)=>{
-    setSearch(e.target.value);
+    props.setSearch(e.target.value);
   };
   
   const handleSubmit = (e)=>{
     e.preventDefault();
-    console.log(search)
+    console.log(props.search)
   };
   
   return (
     <form onClick={handleSubmit}>
-        <input type='text' value={search} onChange={handleInputChange} placeholder='Search for recipe'/>
+        <input type='text' value={props.search} onChange={handleInputChange} placeholder='Enter Ingredients ...'/>
         <button type='submit'>Submit</button>
     </form>
   )
